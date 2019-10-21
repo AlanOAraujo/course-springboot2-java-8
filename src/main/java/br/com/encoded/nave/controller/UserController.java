58f20c60087949bab8c69a5f31b272c8ad2db70c
Alan Araujo <alanoliveiracx@gmail.com>
2019-10-21 14:39:57 -0300

    User Entite and Controller

---
+++ src/main/java/br/com/encoded/nave/controller/UserController.java
@@ -0,0 +1,20 @@
+package br.com.encoded.nave.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.encoded.nave.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1, "Alan", "alanolive@gmail", "119999999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
