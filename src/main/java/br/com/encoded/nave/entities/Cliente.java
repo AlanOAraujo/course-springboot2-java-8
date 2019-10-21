2cecba74b0623d655910d61955f698441764ec06
Alan Araujo <alanoliveiracx@gmail.com>
2019-10-21 17:19:48 -0300

    Order, Instant, ISO 8601, Cliente

--- src/main/java/br/com/encoded/nave/entities/Cliente.java
+++ src/main/java/br/com/encoded/nave/entities/Cliente.java
@@ -1,5 +1,6 @@
 package br.com.encoded.nave.entities;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -7,12 +8,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
-//@Entity
-public class Cliente {
+@Entity
+@Table(name = "tb_cliente")
+public class Cliente implements Serializable {
+	private static final long serialVersionUID = 1L;
 
-//	@Id
-//	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	
 	private String nome;
@@ -63,6 +67,31 @@ public class Cliente {
 
 	public void setDt_nascimento(LocalDate dt_nascimento) {
 		this.dt_nascimento = dt_nascimento;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 	
 }
